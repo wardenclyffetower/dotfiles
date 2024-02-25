4f530f03d84f4f4acf3ad87ae723a24f2878fba2
Warden Clyffe <warden-clyffe-tower@tuta.io>
2024-02-25 16:43:09 +0100

    Added background for every kind of highlight groups on neovim

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,14 +1,18 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	enabled = true,
-	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			transparent = true,
-			terminal_colors = true,
-		})
-		vim.cmd([[colorscheme tokyonight]])
-	end,
+  "folke/tokyonight.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  enabled = true,
+  config = function()
+    require("tokyonight").setup({
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        float = "transparent",
+      },
+      terminal_colors = true,
+    })
+    vim.cmd([[colorscheme tokyonight]])
+  end,
 }
