63869e39da7b94879481ec5273b8a2645126566b
Warden Clyffe <warden-clyffe-tower@tuta.io>
2024-03-18 00:05:08 +0100

    At last some cool configuration for nvim-cmp

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -16,3 +16,40 @@ vim.o.spelllang = "fr"
 vim.o.spell = true
 
 vim.o.redrawtime = 3500 -- increase from 2000, to let some Telescope searches highlight
+
+vim.o.pumheight = 15 -- also used on nvim-cmp
+vim.o.pumblend = 0 -- no transparency on menu of completions
+
+-- Customization for Pmenu (come from nvim-cmp documentation)
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
+vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
+
+vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+
+vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
+vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,26 +2,71 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "hrsh7th/cmp-emoji",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
     "f3fora/cmp-spell",
     "andersevenrud/cmp-tmux",
     "lukas-reineke/cmp-under-comparator",
+    "onsails/lspkind.nvim",
   },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     local cmp = require("cmp")
-    table.insert(opts.sources, { name = "spell", label = "[spell]" })
-    table.insert(opts.sources, { name = "tmux", label = "[tmux]" })
-    table.insert(opts.sources, { name = "emoji", label = "[emoji]" })
-
+    -- Reset the table of sources (?)
+    for k in pairs(opts.sources) do
+      opts.sources[k] = nil
+    end
+    table.insert(opts.sources, { name = "nvim_lsp", label = "[lsp]", priority = 8, group_index = 1 })
+    table.insert(
+      opts.sources,
+      { name = "luasnip", label = "[snippet]", priority = 7, max_item_count = 3, group_index = 1 }
+    )
+    table.insert(
+      opts.sources,
+      { name = "buffer", label = "[buffer]", priority = 8, keyword_length = 4, group_index = 2 }
+    )
+    table.insert(
+      opts.sources,
+      { name = "nvim_lsp_signature_help", label = "[lsp signature]", priority = 8, group_index = 1 }
+    )
+    table.insert(opts.sources, { name = "spell", label = "[spell]", priority = 6, group_index = 3 })
+    table.insert(opts.sources, { name = "tmux", label = "[tmux]", priority = 6, keyword_length = 4, group_index = 2 })
+    table.insert(opts.sources, { name = "path", label = "[path]", priority = 5, group_index = 2 })
+    table.insert(opts.sources, { name = "emoji", label = "[emoji]", priority = 3, group_index = 3 })
     -- changing comparators
     -- ref : https://github.com/gennaro-tedesco/dotfiles/blob/4a175cce9f8f445543ac61cc6c4d6a95d6a6da10/nvim/lua/plugins/cmp.lua#L79-L88
-    opts.sorting.comparators = {
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
-      cmp.config.compare.recently_used,
-      require("cmp-under-comparator").under, -- ease compare on __ functions
-      cmp.config.compare.kind,
+    opts.sorting = {
+      priority_weight = 2.0,
+      comparators = {
+        cmp.config.compare.locality,
+        cmp.config.compare.recently_used,
+        cmp.config.compare.score,
+        cmp.config.compare.offset,
+        cmp.config.compare.order,
+        cmp.config.compare.exact,
+        require("cmp-under-comparator").under, -- ease compare on __ functions
+        -- cmp.config.compare.kind,
+      },
+    }
+
+    opts.window = {
+      completion = cmp.config.window.bordered({
+        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+        col_offset = -3,
+        side_padding = 0,
+        scrollbar = true,
+      }),
+      documentation = cmp.config.window.bordered(),
+    }
+    opts.formatting = {
+      expandable_indicator = true,
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, vim_item)
+        local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+        local strings = vim.split(kind.kind, "%s", { trimempty = true })
+        kind.kind = " " .. (strings[1] or "") .. " "
+        kind.menu = "    (" .. (strings[2] or "") .. ")"
+        return kind
+      end,
     }
   end,
 }
