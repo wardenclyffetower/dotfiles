43da9609b3285b544534fea4bc30e0fae7d6133d
Warden Clyffe <warden-clyffe-tower@tuta.io>
2024-03-18 21:01:02 +0100

    Adding tpope plugins (Commentary)

---
+++ .config/nvim/lua/plugins/pope.lua
@@ -0,0 +1,8 @@
+return {
+
+  { "tpope/vim-sensible", enabled = false, lazy = false },
+  { "tpope/vim-repeat", enabled = false, lazy = false },
+  { "tpope/vim-surround", enabled = false, lazy = false },
+  { "tpope/vim-commentary", cmd = { "Commentary" } },
+  { "tpope/vim-unimpaired", enabled = false, lazy = false },
+}
