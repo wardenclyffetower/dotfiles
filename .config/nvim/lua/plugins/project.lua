110292a5f0e404b48364babc0253b70817adf89d
Warden Clyffe <warden-clyffe-tower@tuta.io>
2024-05-08 20:20:19 +0100

    Project lazy loading changed

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,11 +1,12 @@
 return {
-	"ahmedkhalf/project.nvim",
-	keys = { {
-		"<leader>tp",
-		"<cmd>:Telescope projects<cr>",
-		desc = "Projects",
-	} },
-	config = function()
-		require("project_nvim").setup()
-	end,
+  "ahmedkhalf/project.nvim",
+  lazy = false,
+  -- keys = { {
+  --   "<leader>tp",
+  --   "<cmd>:Telescope projects<cr>",
+  --   desc = "Projects",
+  -- } },
+  config = function()
+    require("project_nvim").setup()
+  end,
 }
